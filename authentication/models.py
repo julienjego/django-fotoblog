d7333ff99d4cf04de22b7ab000affcd500edbb44
julienjego <jego.jj@gmail.com>
2023-01-10 14:22:48 +0100

    add User and authentication functions and pages

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    # Extension de AbstractUser pour profiter du modèle User et de
+    # l'authentification de Django
+    CREATOR = "CREATOR"
+    SUBSCRIBER = "SUBSCRIBER"
+
+    ROLE_CHOICES = ((CREATOR, "Créateur"), (SUBSCRIBER, "Abonné"))
+
+    profile_photo = models.ImageField(verbose_name="Photo de profil")
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name="Rôle")
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
-# Create your views here.
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ""
+    if request.method == "POST":
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                message = f"""Bonjour {user.username} ! 
+                Vous êtes bien connecté."""
+                login(request, user)
+            else:
+                message = "Identifiants invalides."
+
+    return render(
+        request, "authentication/login.html", {"form": form, "message": message}
+    )
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
