06c4ec62b9f9475567c4407dd59f5831d23a3684
julienjego <jego.jj@gmail.com>
2023-01-10 15:22:25 +0100

    switch to classview for login

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import View
 from . import forms
 
 
-def login_page(request):
-    form = forms.LoginForm()
-    message = ""
-    if request.method == "POST":
-        form = forms.LoginForm(request.POST)
+class LoginPageView(View):
+    template_name = "authentication/login.html"
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ""
+        return render(request, self.template_name, {"form": form, "message": message})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             user = authenticate(
                 username=form.cleaned_data["username"],
@@ -19,9 +26,30 @@ def login_page(request):
 
         message = "Identifiants invalides."
 
-    return render(
-        request, "authentication/login.html", {"form": form, "message": message}
-    )
+        return render(
+            request, "authentication/login.html", {"form": form, "message": message}
+        )
+
+
+# def login_page(request):
+#     form = forms.LoginForm()
+#     message = ""
+#     if request.method == "POST":
+#         form = forms.LoginForm(request.POST)
+#         if form.is_valid():
+#             user = authenticate(
+#                 username=form.cleaned_data["username"],
+#                 password=form.cleaned_data["password"],
+#             )
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect("home")
+
+#         message = "Identifiants invalides."
+
+#     return render(
+#         request, "authentication/login.html", {"form": form, "message": message}
+#     )
 
 
 def logout_user(request):
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-@login_required
-def home(request):
-    return render(request, "blog/home.html")
+class HomeView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, "blog/home.html")
+
+
+# @login_required
+# def home(request):
+#     return render(request, "blog/home.html")
